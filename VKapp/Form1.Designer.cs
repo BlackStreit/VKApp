ca09b772db4e4b5f65cdb0406cf2b8dd24fcf9c4
Иван <mix06062001@outlook.com>
2021-02-15 09:55:40 +0800

    Добавьте файлы проекта.

---
+++ VKapp/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace VKapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMakeRequests = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnMakeRequests
+            // 
+            this.btnMakeRequests.Location = new System.Drawing.Point(57, 91);
+            this.btnMakeRequests.Name = "btnMakeRequests";
+            this.btnMakeRequests.Size = new System.Drawing.Size(117, 48);
+            this.btnMakeRequests.TabIndex = 0;
+            this.btnMakeRequests.Text = "Сделать запросс";
+            this.btnMakeRequests.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "166766754";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnMakeRequests);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMakeRequests;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
